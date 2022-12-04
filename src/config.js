9737b4bc6fd1a4a03abb8ab16b60d481e23f6cd7
M Powers <mpowers@alum.mit.edu>
2022-12-04 14:00:36 -0500

    Update some config values for demo

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // https://stackoverflow.com/questions/34351804/how-to-declare-a-global-variable-in-react
 module.exports = global.config = {
 	// tick_interval: 30000,
-	tick_interval: 2000,
-	// max_ticks: 240,
-	max_ticks: 5,
+	tick_interval: 2500,
+	max_ticks: 240,
+	// max_ticks: 5,
 	salary_cap: 20,
 	player_cap: 24,
 	bid_min: .05,
