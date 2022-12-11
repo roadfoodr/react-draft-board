1811922fd01f15815df0202ab5004e21986db1c3
M Powers <mpowers@alum.mit.edu>
2022-12-11 17:39:02 -0500

    Rework to use realtime firestore listener instead of periodic db poll

--- src/Board.js
+++ src/Board.js
@@ -1,10 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import { useParams } from 'react-router-dom';
-import {db} from "./firebase"
-import {collection, getDocs, orderBy} from "firebase/firestore"
+import {useParams} from 'react-router-dom';
+import {db} from "./firebase";
+import {collection, onSnapshot} from "firebase/firestore";
 
 import Header from './Components/Header.js';
-import UpdateControls from './Components/UpdateControls.js';
 import Team from './Components/Team.js';
 
 const currency = (n) => { n=parseFloat(n); return isNaN(n) ? false : n.toFixed(2); }
@@ -14,10 +13,6 @@ const maybenum = (n) => { return isNaN(Number(n)) ? n : Number(n); }
 
 function Board() {
   const urlParams = useParams();
-
-  const [tick, setTick] = useState(0);
-  const [tickIntervalID, setTickIntervalID] = useState(null);
-  const [tickPaused, setTickPaused] = useState(true);
 
   const [players, setPlayers] = useState([]);
   const [franchises, setFranchises] = useState([]);
@@ -34,75 +29,53 @@ function Board() {
             (maybenum(f1[sortField]) >= maybenum(f2[sortField]) ? 1 : -1) * sortAscend);
     }
 
+  // Only establish the snapshot listener at first render
   useEffect(() => {
-    // Start the tick sequence for the first time
-    setTickPaused(false);
-  }, []);
 
-  useEffect(() => {
-    if (!tickPaused) {
-      const interval = setInterval(() => {
-        setTick(tick => tick + 1);
-      }, global.config.tick_interval);
-      setTickIntervalID(interval);
-    } else {
-      clearInterval(tickIntervalID);
-    }
-  }, [tickPaused]);
-
-  useEffect(() => {
-    console.log("inside tick useEffect, tick = " + tick);
-    // read all players from db
-    let players_temp = [];
-    let franchise_set = new Set();
     const thisYear = urlParams.hasOwnProperty('year') ? urlParams.year : new Date().getFullYear();
-    const collRef = `years/${thisYear}/players`;
-    getDocs(collection(db, collRef), 
-      orderBy("nameLast"), orderBy("nameFirst"), orderBy("position"), orderBy("team"), 
-      ).then((snapshot) => {
-    snapshot.forEach((doc) => {
-      let player_temp = doc.data();
-      player_temp.name = player_temp.nameLast + ", " + player_temp.nameFirst;
-      player_temp.salary = currency(player_temp.salary);
-      players_temp.push(player_temp);
-      franchise_set.add(doc.data().franchise);
-    });
+    const collRef = collection(db, `years/${thisYear}/players`);
 
-    // Compute summaries for each franchise
-    let franchise_temp = [...franchise_set].sort().map( franchise => ({"franchise": franchise}) );
-    franchise_temp.forEach( franchise => {
-      let franchise_players = players_temp.filter( player => player.franchise === franchise.franchise)
-      franchise.player_count = franchise_players.length;
-      franchise.player_count_off = franchise_players.filter( player => 
-          ["QB", "RB", "WR", "TE", "WRTE"].includes(player.position) ).length;
-      franchise.player_count_dst = franchise_players.filter( player => 
-          ["K", "LB", "DE", "DB"].includes(player.position) ).length;
-      franchise.spent = currency(franchise_players.reduce((accumulator, object) => {
-          return accumulator + Number(object.salary); }, 0) );
-      franchise.remain = currency(global.config.salary_cap - franchise.spent);
-      franchise.maxbid = currency(global.config.salary_cap - franchise.spent 
-          - ((global.config.player_cap - franchise.player_count) * global.config.bid_min) 
-          + global.config.bid_min);
-      franchise.total_rating = wholenum(franchise_players.reduce((accumulator, object) => {
-          return accumulator + Number(object.rating); }, 0) );
+    const unsub_listener = onSnapshot(collRef, (snapshot) => {
+      console.log("Current snapshot: ", snapshot);
+      let players_temp = [];
+      let franchise_set = new Set();
+
+      snapshot.forEach((doc) => {
+        let player_temp = doc.data();
+        player_temp.name = player_temp.nameLast + ", " + player_temp.nameFirst;
+        player_temp.salary = currency(player_temp.salary);
+        players_temp.push(player_temp);
+        franchise_set.add(doc.data().franchise);
       });
 
-    // Need to explicitly prevent "equal" reassignment because the default Object.js comparison is not deep
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/is
-    // https://masteringjs.io/tutorials/fundamentals/compare-arrays
-    // console.log('new: ' + JSON.stringify(franchise_temp));
-    // console.log('old: ' + JSON.stringify(franchises));
-    if (JSON.stringify(players_temp) !== JSON.stringify(players)) {
-      setPlayers(players_temp);
-    }
-    if (JSON.stringify(franchise_temp) !== JSON.stringify(franchises)) {
-      setFranchises(franchise_temp);
-    }
+      // Compute summaries for each franchise
+      let franchise_temp = [...franchise_set].sort().map( franchise => ({"franchise": franchise}) );
+      franchise_temp.forEach( franchise => {
+        let franchise_players = players_temp.filter( player => player.franchise === franchise.franchise)
+        franchise.player_count = franchise_players.length;
+        franchise.player_count_off = franchise_players.filter( player => 
+            ["QB", "RB", "WR", "TE", "WRTE"].includes(player.position) ).length;
+        franchise.player_count_dst = franchise_players.filter( player => 
+            ["K", "LB", "DE", "DB"].includes(player.position) ).length;
+        franchise.spent = currency(franchise_players.reduce((accumulator, object) => {
+            return accumulator + Number(object.salary); }, 0) );
+        franchise.remain = currency(global.config.salary_cap - franchise.spent);
+        franchise.maxbid = currency(global.config.salary_cap - franchise.spent 
+            - ((global.config.player_cap - franchise.player_count) * global.config.bid_min) 
+            + global.config.bid_min);
+        franchise.total_rating = wholenum(franchise_players.reduce((accumulator, object) => {
+            return accumulator + Number(object.rating); }, 0) );
+        });
 
-    // as a sanity measure, automatically pause updates after a while
-    if (tick > global.config.max_ticks) { setTickPaused(true); }
-    })
-  }, [tick])
+      setPlayers(players_temp);
+      setFranchises(franchise_temp);
+    },
+      (error) => {
+        console.log("Listen error: ", error);
+      });
+
+  }, []);
+
 
   useEffect(() => {
     if (anchorTarget) {
@@ -112,15 +85,10 @@ function Board() {
   }}, [anchorTarget]);
 
 
-  // TODO: need useMemo to prevent Board re-render when only tick changes 
   return (
     <div className="content-wrapper">
 
       <Header players={players} num_franchises={franchises.length}/>
-
-      <UpdateControls 
-          tick={tick} tickPaused={tickPaused}
-          setTick={setTick} setTickPaused={setTickPaused} />
 
       <div className="pure-g is-center">
         {franchises.sort(franchise_sort_func).map((franchise, i) =>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import App from './App';
+// import App from './App';
 import Add from './Add';
 import Board from './Board';
 
